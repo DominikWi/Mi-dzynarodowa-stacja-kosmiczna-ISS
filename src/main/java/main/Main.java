c2c3efdf6fc9c4cb2d6cf76ce290d87ff84d86a8
norbi999 <69053304+norbi999@users.noreply.github.com>
2021-01-09 13:56:20 +0100

    json PeopleInSpace Lista

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
+import api.PeopleList;
 import database.entity.IssLocation;
 import database.utils.HibernateUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,9 +14,11 @@ import static api.JsonReader.readJsonFromUrl;
 
 public class Main {
 
-    public static void main(String[] args)  throws IOException, JSONException {
-            JSONObject json = readJsonFromUrl("http://api.open-notify.org/astros.json");
+    private static JSONArray obj;
 
+    public static void main(String[] args)  throws IOException, JSONException {
+        JSONObject jsonAstronauci = readJsonFromUrl("http://api.open-notify.org/astros.json");
+        JSONObject jsonLokalizacja = readJsonFromUrl("http://api.open-notify.org/iss-now.json");
 
             HibernateUtils
                 .getInstance()
@@ -23,27 +27,30 @@ public class Main {
         System.out.println("a - Podaj predkosc ISS");
         System.out.println("b - Wyswietl liste nadchodzacych przebiegow ISS dla okreslonej lokalizacji");
         System.out.println("c - Wyswietl liczbe osob przebywajacych w kosmosie w ramach ISS");
-        System.out.println("d - Wyjdz");
+        System.out.println("d - Wyswietl liste osob przebywajacych w kosmosie w ramach ISS");
+        System.out.println("e - Wyjscie");
         Scanner scan = new Scanner(System.in);
         String choice = scan.nextLine();
         switch (choice) {
             case "a":
-                System.out.println("Predkosc ISS wynosi: ");
+                System.out.println("Predkosc ISS wynosi ~ 28,000 km/h ");
                 break;
             case "b":
-                IssLocation location = new IssLocation();
-                System.out.println(location);
+              //lokalizacja do ogarniecia
 
                 break;
             case "c":
+                System.out.println("W kosmosie przebywa " +jsonAstronauci.get("number")+ " osob.") ;
+            case "d":
 
-                System.out.println("W kosmosie przebywa " +json.get("number")+ " osob.") ;
+                PeopleList peopleList=new PeopleList();
 
-/*
-                System.out.println(json.toString()); do zedytowania lista zalogi
-*/
+                System.out.println(peopleList);
 
                 break;
+
+            case "e":
+                System.out.println("Ciao");
             default:
                 System.out.println("Zły wybor");
         }
